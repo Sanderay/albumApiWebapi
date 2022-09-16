85f5195328e6ad3454f673d9f85554da5c8c6466
Ray Sanders <ray.sanders@wwt.com>
2022-09-16 12:54:11 -0500

    Update .gitignore, change test name

--- AlbumApiWebapi.Tests/AlbumController.Tests.cs
+++ AlbumApiWebapi.Tests/AlbumController.Tests.cs
@@ -1,10 +1,11 @@
-namespace AlbumApiWebapi.Tests;
+namespace AlbumApiWebapi;
 
 public class AlbumController
 {
     [Fact]
     public void Get_ReturnsRvsAlbums()
     {
+
         Assert.Equal(4, 4);
     }
 }
